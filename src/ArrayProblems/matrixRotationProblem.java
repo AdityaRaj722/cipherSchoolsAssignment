4d5da078a6ba8abeb0ef67ab16709754f97a1bb0
AdityaRaj722 <aditya0722004@gmail.com>
2024-07-18 00:01:35 +0530

    Third

--- src/ArrayProblems/matrixRotationProblem.java
+++ src/ArrayProblems/matrixRotationProblem.java
@@ -22,7 +22,7 @@ public class matrixRotationProblem {
                 {7, 8, 9}
         };
         reverse(matrix);
-        for (int i = 0; i < matrix.length; i++) {
+        for(int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
                 System.out.print(matrix[i][j] + " ");
             }
